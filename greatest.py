cd75476044f35eeae95dc53f4d1ccb4abdc0b3c2
lokelokss <46619567+lokelokss@users.noreply.github.com>
2019-02-07 00:50:42 -0800

    Update greatest.py

--- greatest.py
+++ greatest.py
@@ -1,10 +1,11 @@
-number1 = 1
-number2 = 2
-number3 = 3
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
 if (number1 >= number2) and (number1 >= number3):
    largest = number1
 elif (number2 >= number1) and (number2 >= number3):
    largest = number2
 else:
    largest = number3
+
 print(largest)
